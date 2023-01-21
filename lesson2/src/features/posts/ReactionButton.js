087cc82b150aaf02fd91ad7b0a369c7f5ac8be71
Steve Clair <sclair@bird-technologies.com>
2023-01-20 23:15:16 -0500

    commit 1

---
+++ lesson2/src/features/posts/ReactionButton.js
@@ -0,0 +1,36 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postSlice";
+
+const reactionEmoji={
+    thumbsUp: '👍',
+    hooray: '🎉',
+    heart: '❤️',
+    rocket: '🚀',
+    eyes: '👀'
+
+}
+
+
+
+const ReactionButton = ({ post }) => {
+    const dispatch = useDispatch()
+    const reactionButtons = Object.entries(reactionEmoji).map(([name,emoji])=>{
+        return(
+            <button
+                className="reactionButton"
+                key={name}
+                type='button'
+                onClick={()=> dispatch(reactionAdded({postId: post.id, reaction: name}))}
+            >
+                {emoji}{post.reactions[name]}
+            </button>
+        )
+    })
+  return (
+    <div>
+        {reactionButtons}
+    </div>
+  )
+}
+
+export default ReactionButton
